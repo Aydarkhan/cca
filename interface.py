33dd09228349c4b31a88722a6b0bc2fb29d2016f
Ruslan Aydarkhanov <Aydarkhan@users.noreply.github.com>
2015-03-12 15:12:46 +0100

    Proper project descriptions in files

--- interface.py
+++ interface.py
@@ -1,19 +1,19 @@
 """Copyright 2010 Aydarkhanov Ruslan, Kurochkin Ilya, Rusinov Ivan
 
-This file is part of Foobar.
+This file is part of CCA.
 
-Foobar is free software: you can redistribute it and/or modify it under
+CCA is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published
 by the Free Software Foundation, either version 2 of the License,
 or (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+CCA is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty
 of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar. If not, see http://www.gnu.org/licenses/.
+along with CCA. If not, see http://www.gnu.org/licenses/.
 """
 
 import math
